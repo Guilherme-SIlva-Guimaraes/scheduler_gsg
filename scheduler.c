e9a7e1321e9cbe8841d7e475934e439f22fdc30e
Guilherme Guimarães <gg92821414@gmail.com>
2026-09-08 22:39:20 -0300

    bloco 3: motor de simulacao por evento discreto, validado contra o gabarito do enunciado

--- scheduler.c
+++ scheduler.c
@@ -12,7 +12,30 @@ typedef struct {
     int periodo;
     int deadline;
     int burst;
+
+    int proxima_chegada;
+    int prazo_absoluto;
+    int burst_restante;
+    int ativa;
+
+    int concluidas;
+    int perdidas;
+    int morta;
 } Tarefa;
+
+typedef struct {
+    int indice_tarefa;
+    int duracao;
+    char razao;
+} Segmento;
+
+typedef struct {
+    Segmento *segmentos;
+    int quantidade;
+    int capacidade;
+} Historico;
+
+typedef int (*FuncaoPrioridade)(const Tarefa *tarefas, int indice);
 
 void erro_fatal(const char *msg) {
     fprintf(stderr, "erro: %s\n", msg);
@@ -164,6 +187,166 @@ Tarefa *ler_tarefas(FILE *f, int *quantidade) {
     return tarefas;
 }
 
+void inicializar_estado_simulacao(Tarefa *tarefas, int quantidade) {
+    for (int i = 0; i < quantidade; i++) {
+        tarefas[i].proxima_chegada = 0;
+        tarefas[i].prazo_absoluto = 0;
+        tarefas[i].burst_restante = 0;
+        tarefas[i].ativa = 0;
+        tarefas[i].concluidas = 0;
+        tarefas[i].perdidas = 0;
+        tarefas[i].morta = 0;
+    }
+}
+
+int obter_prioridade_por_periodo(const Tarefa *tarefas, int indice) {
+    return tarefas[indice].periodo;
+}
+
+int selecionar_tarefa(Tarefa *tarefas, int quantidade, FuncaoPrioridade prioridade) {
+    int escolhida = -1;
+    int melhor_chave = 0;
+
+    for (int i = 0; i < quantidade; i++) {
+        if (!tarefas[i].ativa) {
+            continue;
+        }
+        int chave = prioridade(tarefas, i);
+        if (escolhida == -1 || chave < melhor_chave) {
+            escolhida = i;
+            melhor_chave = chave;
+        }
+    }
+
+    return escolhida;
+}
+
+void adicionar_segmento(Historico *h, int indice_tarefa, int duracao, char razao) {
+    if (h->quantidade == h->capacidade) {
+        h->capacidade = (h->capacidade == 0) ? 8 : h->capacidade * 2;
+        Segmento *tmp = realloc(h->segmentos, h->capacidade * sizeof(Segmento));
+        if (tmp == NULL) {
+            erro_fatal("falha ao realocar memoria de historico");
+        }
+        h->segmentos = tmp;
+    }
+
+    h->segmentos[h->quantidade].indice_tarefa = indice_tarefa;
+    h->segmentos[h->quantidade].duracao = duracao;
+    h->segmentos[h->quantidade].razao = razao;
+    h->quantidade++;
+}
+
+void simular(Tarefa *tarefas, int quantidade, int tempo_total,
+             FuncaoPrioridade prioridade, Historico *historico) {
+    int segmento_tarefa = -1;
+    int segmento_duracao = 0;
+
+    int *perdeu_agora = calloc((size_t) quantidade, sizeof(int));
+    int *acabou_de_terminar = calloc((size_t) quantidade, sizeof(int));
+    if (perdeu_agora == NULL || acabou_de_terminar == NULL) {
+        erro_fatal("falha ao alocar memoria de simulacao");
+    }
+
+    for (int t = 0; t < tempo_total; t++) {
+        for (int i = 0; i < quantidade; i++) {
+            perdeu_agora[i] = 0;
+        }
+
+        for (int i = 0; i < quantidade; i++) {
+            if (tarefas[i].ativa && tarefas[i].prazo_absoluto == t && tarefas[i].burst_restante > 0) {
+                tarefas[i].ativa = 0;
+                tarefas[i].perdidas++;
+                perdeu_agora[i] = 1;
+            }
+        }
+
+        for (int i = 0; i < quantidade; i++) {
+            if (t == tarefas[i].proxima_chegada) {
+                tarefas[i].ativa = 1;
+                tarefas[i].burst_restante = tarefas[i].burst;
+                tarefas[i].prazo_absoluto = t + tarefas[i].deadline;
+                tarefas[i].proxima_chegada += tarefas[i].periodo;
+            }
+        }
+
+        int quem = selecionar_tarefa(tarefas, quantidade, prioridade);
+
+        int houve_transicao = (segmento_tarefa != quem);
+        if (segmento_tarefa != -1 && (acabou_de_terminar[segmento_tarefa] || perdeu_agora[segmento_tarefa])) {
+            houve_transicao = 1;
+        }
+
+        if (houve_transicao) {
+            if (segmento_tarefa != -1) {
+                char razao;
+                if (acabou_de_terminar[segmento_tarefa]) {
+                    razao = 'F';
+                } else if (perdeu_agora[segmento_tarefa]) {
+                    razao = 'L';
+                } else {
+                    razao = 'H';
+                }
+                adicionar_segmento(historico, segmento_tarefa, segmento_duracao, razao);
+            } else if (segmento_duracao > 0) {
+                adicionar_segmento(historico, -1, segmento_duracao, '\0');
+            }
+            segmento_tarefa = quem;
+            segmento_duracao = 0;
+        }
+
+        for (int i = 0; i < quantidade; i++) {
+            acabou_de_terminar[i] = 0;
+        }
+
+        if (quem != -1) {
+            tarefas[quem].burst_restante--;
+            segmento_duracao++;
+            if (tarefas[quem].burst_restante == 0) {
+                tarefas[quem].ativa = 0;
+                tarefas[quem].concluidas++;
+                acabou_de_terminar[quem] = 1;
+            }
+        } else {
+            segmento_duracao++;
+        }
+    }
+
+    if (segmento_tarefa != -1) {
+        char razao;
+        if (acabou_de_terminar[segmento_tarefa]) {
+            razao = 'F';
+        } else {
+            razao = 'K';
+            tarefas[segmento_tarefa].morta = 1;
+        }
+        adicionar_segmento(historico, segmento_tarefa, segmento_duracao, razao);
+    } else if (segmento_duracao > 0) {
+        adicionar_segmento(historico, -1, segmento_duracao, '\0');
+    }
+
+    for (int i = 0; i < quantidade; i++) {
+        if (tarefas[i].ativa) {
+            tarefas[i].morta = 1;
+        }
+    }
+
+    free(perdeu_agora);
+    free(acabou_de_terminar);
+}
+
+void imprimir_historico_debug(const Tarefa *tarefas, const Historico *h) {
+    for (int i = 0; i < h->quantidade; i++) {
+        Segmento s = h->segmentos[i];
+        if (s.indice_tarefa == -1) {
+            printf("idle for %d units\n", s.duracao);
+        } else {
+            printf("[%s] for %d units - %c\n",
+                   tarefas[s.indice_tarefa].nome, s.duracao, s.razao);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     validar_argumentos(argc, argv);
 
@@ -175,14 +358,29 @@ int main(int argc, char **argv) {
 
     fclose(f);
 
-    (void) tempo_total;
+    inicializar_estado_simulacao(tarefas, quantidade);
 
+    Historico historico = { NULL, 0, 0 };
+    simular(tarefas, quantidade, tempo_total, obter_prioridade_por_periodo, &historico);
+
+    imprimir_historico_debug(tarefas, &historico);
+
+    printf("\nLOST DEADLINES\n");
     for (int i = 0; i < quantidade; i++) {
-        printf("%s periodo=%d deadline=%d burst=%d\n",
-               tarefas[i].nome, tarefas[i].periodo,
-               tarefas[i].deadline, tarefas[i].burst);
+        printf("[%s] %d\n", tarefas[i].nome, tarefas[i].perdidas);
     }
 
+    printf("\nCOMPLETE EXECUTION\n");
+    for (int i = 0; i < quantidade; i++) {
+        printf("[%s] %d\n", tarefas[i].nome, tarefas[i].concluidas);
+    }
+
+    printf("\nKILLED\n");
+    for (int i = 0; i < quantidade; i++) {
+        printf("[%s] %d\n", tarefas[i].nome, tarefas[i].morta);
+    }
+
+    free(historico.segmentos);
     free(tarefas);
     return 0;
 }
